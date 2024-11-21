e85888718e4c5836b7d87bd398f4a35ff90d8183
David Adriazola <d.adriazolamuriel@gmail.com>
2024-11-21 18:41:14 -0400

    MATRIX position

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [matrix, setMatrix] = useState<number[][]>([])
+ //slide puzzle game
 
+
+  useEffect(() => {
+   setMatrix([[1, 2, 3], [4, 0, 6], [0, 8, 9]])
+
+
+  }, [])
+  // const emptyNeighbor = (value:number) => {
+    
+    
+  // }
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    const target = event.target as HTMLElement;
+    const value = target.innerText;
+    if (target.innerText === '0') {
+      console.log('empty cell');
+    } else {
+      console.log('not empty cell');
+      // emptyNeighbor(parseInt(value));
+      console.log(value);
+      console.log("Position",target.dataset.rowIndex, target.dataset.colIndex);
+    }
+    
+  }
+
+ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <main>
+        <table>
+          <tbody>
+            {matrix.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, colIndex) => (
+                  <td 
+                  onClick={handleClick} 
+                  key={`${rowIndex}-${colIndex}`} 
+                  data-row-index={rowIndex}
+                  data-col-index={colIndex}
+                  >{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </>
+      
   )
 }
 
